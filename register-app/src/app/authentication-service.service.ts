ad56be3a5a258a475f0d453ae12334e6b43e2643
Daniel McAuliffe <danielmcauliffe@outlook.com>
2019-01-22 23:25:03 +1100

    change path

--- register-app/src/app/authentication-service.service.ts
+++ register-app/src/app/authentication-service.service.ts
@@ -4,7 +4,7 @@ import { Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
-const apiUrl: string = "http://localhost:3000/api/";
+const apiUrl: string = "http://ec2-52-14-82-165.us-east-2.compute.amazonaws.com/api/";
 
 export interface UserDetails {
   _id: string;
